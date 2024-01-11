fe338bfc58c17a76bafd9699ceb264cb19b86f49
jean carllo <jeanrosete04@gmail.com>
2024-01-11 15:27:55 -0300

    dificuldade em exporta, tirar duvidas.

---
+++ Atividades_HTML/exercicioReact/src/Components/Cabecalho/index.jsx
@@ -0,0 +1,18 @@
+import './cabecalho.css'
+
+const Testando = () => {
+    return (
+        <header>
+<div className="container ">
+    <h1 className='ajuste'>Calculadora IMC</h1>
+    <p className='ajuste' >Calcule seu IMC, basta inserir altura e peso, o resto eu calculo!!</p>
+    <div>
+        <img src="https://www.drrogermoura.com.br/images/artigos/tabela-imc.png" alt="tabela de informações sobre IMC" />
+    </div>
+</div>
+</header> 
+    )
+
+}
+
+export default Testando;

c71332b0162a636f6d45438d34fc8e165a79231c
jean carllo <jeanrosete04@gmail.com>
2024-01-11 14:57:36 -0300

    incerteza sobre as exportações.

--- Atividades_HTML/exercicioReact/src/App.jsx
+++ Atividades_HTML/exercicioReact/src/App.jsx
@@ -1,14 +1,25 @@
 
+
 function App() {
   return (
     <div>
         <header>
           <div className="container">
-          <h1>Calculadora IMC</h1>
+            <h1>Calculadora IMC</h1>
+          <div>
+            <img src="https://www.drrogermoura.com.br/images/artigos/tabela-imc.png" alt="tabela de informações sobre IMC" />
+          </div>
           </div>
         </header> 
         <div>
-          <img src="https://www.drrogermoura.com.br/images/artigos/tabela-imc.png" alt="" />
+          <form action="">
+            <input type="text" placeholder="Nome" />
+            <input type="number" placeholder="Altura" />
+            <input type="number" placeholder="Peso" />
+            <div>
+            <label htmlFor="">Seu IMC é de 24, Seu peso é ideial (parabens) </label>
+            </div>
+          </form>
         </div>
     </div>
   )

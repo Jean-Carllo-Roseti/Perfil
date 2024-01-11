763d62406980da30f7b3c2d90465dc9f85b4165b
jean carllo <jeanrosete04@gmail.com>
2024-01-11 14:07:06 -0300

    efetuando o set up do projeto, inicializando cabeçalho.

---
+++ Atividades_HTML/exercicioReact/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div>
+        <header>
+          <div className="container">
+          <h1>Calculadora IMC</h1>
+          </div>
+        </header> 
+        <div>
+          <img src="https://www.drrogermoura.com.br/images/artigos/tabela-imc.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default App;

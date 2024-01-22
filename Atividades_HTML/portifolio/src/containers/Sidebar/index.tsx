7cc51d5dc9bce3937f9ac87033988c1051a6969a
jean carllo <jeanrosete04@gmail.com>
2024-01-22 10:02:19 -0300

    estilizando sideBar.

--- Atividades_HTML/portifolio/src/containers/Sidebar/index.tsx
+++ Atividades_HTML/portifolio/src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { SideBarContainer, Descricao, BotaoTema } from "./styles"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ogiansouza.png" alt="" />
-    <Titulo fontSize={20}>Gian Souza</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gian Souza</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ogiansouza
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
